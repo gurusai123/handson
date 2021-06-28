0085691a6148016e7073d318c6b71978a5829d9e
gurusai <51899461+gurusai123@users.noreply.github.com>
2021-06-28 19:15:44 +0530

    Add files via upload

---
+++ 916982_guru_sai-day_67/spring-learn/src/main/java/com/cognizant/springlearn/entity/Country.java
@@ -0,0 +1,48 @@
+package com.cognizant.springlearn.entity;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class Country {
+	// logger
+	private static final Logger LOGGER = LoggerFactory.getLogger(Country.class);
+	
+	@NotNull
+	@Size(min=2, max=2, message="Country code should be 2 characters")
+	private String code;
+	private String name;
+	
+	
+	public Country() {
+		super();
+		LOGGER.debug("“Inside Country Constructor.");
+	}
+
+	public String getCode() {
+		LOGGER.debug("Inside code getter");
+		return code;
+	}
+	
+	public void setCode(String code) {
+		LOGGER.debug("Inside code setter");
+		this.code = code;
+	}
+	
+	public String getName() {
+		LOGGER.debug("Inside name getter");
+		return name;
+	}
+	
+	public void setName(String name) {
+		LOGGER.debug("Inside name setter");
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Country [code=" + code + ", name=" + name + "]";
+	}
+	
+}
